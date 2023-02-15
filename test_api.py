9f84a579f131c24f7f23597d51867050a15c7e98
Nathan Gearke <69879313+GearsandKeys@users.noreply.github.com>
2023-02-15 13:32:27 -0600

    Create test_api.py

---
+++ test_api.py
@@ -0,0 +1,10 @@
+import mock
+import bored
+
+def test_averages(capsys):
+    with mock.patch('builtins.input', side_effect=['key', 5585829]):
+        bored.assignment_13_5()
+        captured = capsys.readouterr()
+        statements = captured.out.split("\n")
+        final_result = statements[-2]
+        assert final_result == "Recommended Activity: Go to an escape room"
