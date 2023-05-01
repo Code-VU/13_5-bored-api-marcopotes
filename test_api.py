76a5ec35e35705076bdf6e5aa9b381829ac370dc
Nathan Gearke <69879313+GearsandKeys@users.noreply.github.com>
2023-05-01 09:59:22 -0500

    Update test_api.py
    Fixed test!

--- test_api.py
+++ test_api.py
@@ -1,10 +1,11 @@
-import mock
+from unittest.mock import patch
 import bored
 
 def test_averages(capsys):
-    with mock.patch('builtins.input', side_effect=['key', 5585829]):
+    with patch('builtins.input', side_effect=['key', 5585829]):
         bored.assignment_13_5()
         captured = capsys.readouterr()
         statements = captured.out.split("\n")
         final_result = statements[-2]
         assert final_result == "Recommended Activity: Go to an escape room"
+        
